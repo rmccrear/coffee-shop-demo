b017a081046d4a4c4ba5b1bd23b53ffb3936759d
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-12-17 22:00:35 -0800

    create product route stubs

--- routes/products.js
+++ routes/products.js
@@ -10,4 +10,32 @@ router.get('/', (req, res) => {
   });
 });
 
+// Get one product
+router.get('/:id', (req, res) => {
+  res.json({
+    message: 'Get one product',
+  });
+});
+
+// Create a product
+router.post('/', (req, res) => {
+  res.json({
+    message: 'Create a product',
+  });
+});
+
+// Update a product
+router.put('/:id', (req, res) => {
+  res.json({
+    message: 'Update a product',
+  });
+});
+
+// Delete a product
+router.delete('/:id', (req, res) => {
+  res.json({
+    message: 'Delete a product',
+  });
+});
+
 module.exports = router;
