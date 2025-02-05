6f7c52a00cd9457f827a42327cfe32c88b04053b
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-02-05 15:39:54 -0800

    update aws to use role to access s3

--- middleware/uploadAWS.js
+++ middleware/uploadAWS.js
@@ -4,48 +4,56 @@ const multerS3 = require('multer-s3');
 const { S3Client } = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+let s3;
 
-// Set storage engine
-const storage = multerS3({
-  s3: s3,
-  bucket: process.env.AWS_S3_BUCKET,
-  key: function (req, file, cb) {
-    cb(
-      null,
-      Date.now().toString() + '-' + file.fieldname + path.extname(file.originalname)
-    );
-  },
-});
-
-// Initialize upload
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-}).single('image');
-
-// Check file type
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif|webp/;
-  const extname = filetypes.test(
-    path.extname(file.originalname).toLowerCase()
-  );
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb('Error: Images Only!');
-  }
+if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+  // use credentials
+  s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+} else {
+  // use IAM role
+  s3 = new S3Client({ region: process.env.AWS_REGION });
 }
 
-module.exports = upload;
+  // Set storage engine
+  const storage = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET,
+    key: function (req, file, cb) {
+      cb(
+        null,
+        Date.now().toString() + '-' + file.fieldname + path.extname(file.originalname)
+      );
+    },
+  });
+
+  // Initialize upload
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
+      checkFileType(file, cb);
+    },
+  }).single('image');
+
+  // Check file type
+  function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif|webp/;
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+
+  module.exports = upload;
